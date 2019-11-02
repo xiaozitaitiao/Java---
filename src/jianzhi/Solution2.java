76188a33a8d31fcb2411fd363cb35bdb954a385b
xiaozitaitiao <1109452106@qq.com>
2019-11-02 15:22:54 +0800

    11

---
+++ src/jianzhi/Solution2.java
@@ -0,0 +1,36 @@
+package jianzhi;
+/**
+ * Day:2
+ * 面试题：42
+ * 类型：动态规划
+ * 题目：连续子数组的最大和
+ * @author 11094
+ *
+ */
+
+public class Solution2 {
+	
+	private static final  int nums[]= {1,-2,3,10,-4,7,2,-5};
+
+	private static int maxSum;
+	public static int getMaxSum(final int nums[])
+	{
+		if(nums==null||nums.length<=0)
+			return 0;
+		int maxSum=nums[0];
+		int curSum=nums[0];
+		for(int i=1;i<nums.length;i++)
+		{
+			if(curSum>=0)curSum+=nums[i];
+			else curSum=nums[i];
+			if(maxSum<curSum)maxSum=curSum;
+
+		}
+		
+		return maxSum;
+	}
+	public static void main(String[] args) {
+		int res=getMaxSum(nums);
+		System.out.println(res);
+	}
+}
---
+++ src/jianzhi/Soultion1.java
@@ -0,0 +1,55 @@
+package jianzhi;
+
+import java.util.Scanner;
+
+public class Soultion1 {
+	public boolean hasPath(char[][]metrixs,int x,int y,int rows,int cols,String target,int len,boolean[][]visited)
+	{
+		if(len==target.length())return true;
+		boolean flag=false;
+		if(x>=0&&x<rows&&y>=0&&y<cols&&visited[x][y]==false&&target.charAt(len)==metrixs[x][y])
+		{
+			visited[x][y]=true;
+			flag=hasPath(metrixs, x-1, y, rows, cols, target, len+1, visited)||
+					hasPath(metrixs, x+1, y, rows, cols, target, len+1, visited)||
+					hasPath(metrixs, x, y-1, rows, cols, target, len+1, visited)||
+					hasPath(metrixs, x, y+1, rows, cols, target, len+1, visited);
+			if(!flag)
+			{
+				visited[x][y]=false;
+				len--;
+			}
+
+		}
+		return flag;
+	}
+	
+	
+public static void main(String[] args) {
+	char[][]metrixs= {
+			{'a','b','t','g'},
+			{'c','f','c','s'},
+			{'j','d','e','h'}
+	};
+	Soultion1 soul= new Soultion1();
+	int rows=3;
+	int cols=4;
+	boolean[][] visited=new boolean[3][4];
+	System.out.println("ÊäÈë×Ö·û´®£º");
+	Scanner scan=new Scanner(System.in);
+	String target=scan.next();
+	boolean res=false;
+	outer:
+	for(int i=0;i<rows;i++)
+		for(int j=0;j<cols;j++)
+		{
+			if(soul.hasPath(metrixs, i,j, rows, cols, target, 0, visited)) {res=true;break outer;}
+		}
+	System.out.println(res);
+	
+		
+	
+	
+			
+}
+}
